7c3f75c992cf3a439bb028097d106a5236f8bcb5
aminabloc <amina@jointhebloc.com>
2024-01-20 12:20:12 -0500

    Building high level job board testing

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,16 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe ('Lever Link Click', () => {
+  beforeAll(async () => {
+    await page.goto("https://jobs.lever.co/altarum")
+  });
+
+  it('should click the first link on Lever', async() => {
+    //click the first link
+      await page.click('a.posting-title')
+
+      await page.waitForTimeout(3000);  
+  });
+});
